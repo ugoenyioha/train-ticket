6f8b988a4ffecd49227a9b100c3d6c2fea421f5e
lincyaw <814750204@qq.com>
2024-05-12 19:56:41 +0800

    add swagger

--- ts-auth-service/src/main/java/auth/AuthApplication.java
+++ ts-auth-service/src/main/java/auth/AuthApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author fdse
  */
+@EnableSwagger2
 @SpringBootApplication
 public class AuthApplication {
     public static void main(String[] args) {
--- ts-verification-code-service/src/main/java/verifycode/VerifyCodeApplication.java
+++ ts-verification-code-service/src/main/java/verifycode/VerifyCodeApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * @author fdse
  */
+@EnableSwagger2
 @SpringBootApplication
 public class VerifyCodeApplication {
     public static void main(String[] args) {
